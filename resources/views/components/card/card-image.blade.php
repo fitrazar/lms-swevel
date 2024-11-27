f5dd17e61a7fe0fa79ce83362cd6413c7082ee20
Zar <fitra6751@gmail.com>
2024-11-27 17:06:17 +0700

    fix: card image

--- resources/views/components/card/card-image.blade.php
+++ resources/views/components/card/card-image.blade.php
@@ -2,7 +2,7 @@
 
 <div {!! $attributes->merge(['class' => 'card bg-base-100 shadow-xl']) !!}>
     <figure>
-        <img src="{{ asset($image) }}" alt="{{ $title ?? 'alt' }}" class="object-contain w-full max-h-44" />
+        <img src="{{ asset($image) }}" alt="{{ $title ?? 'alt' }}" class="object-contain w-full h-56 max-h-56" />
     </figure>
     <div class="card-body">
         <h2 class="card-title">{{ $title ?? '' }}</h2>
