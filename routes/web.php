39581187d632b926c5c71e4cd06abc5aeddc250a
Zar <fitra6751@gmail.com>
2024-12-02 08:07:17 +0700

    fix: profile route

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::prefix('dashboard')->name('dashboard.')->middleware('auth')->group(functi
 
     Route::middleware(['role:participant|instructor'])->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 
