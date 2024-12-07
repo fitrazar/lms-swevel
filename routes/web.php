e48c6e557906412fcf86cd9d5cee3a940892debe
Dheny Cahyono <dhenycahyono1313@gmail.com>
2024-12-07 13:07:53 +0800

    bugs chatify: add participant dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\Admin\AssignmentController;
 use App\Http\Controllers\Admin\EnrollmentController;
 use App\Http\Controllers\Admin\InstructorController;
 use App\Http\Controllers\Admin\ParticipantController;
+use App\Http\Controllers\Participant\SiswaController;
+use App\Http\Controllers\Participant\ParticipantControllerUser;
 use App\Http\Controllers\Admin\CourseController as AdminCourseController;
 use App\Http\Controllers\Instructor\CourseController as InstructorCourseController;
 
@@ -30,7 +32,12 @@ Route::post('/course/{course}/read/done', [CourseController::class, 'completed']
 
 
 Route::prefix('dashboard')->name('dashboard.')->middleware('auth')->group(function () {
+
     Route::get('/', [DashboardController::class, 'index'])->name('index');
+
+    Route::name('participant.')->middleware(['role:participant'])->group(function () {
+        Route::get('/user', [ParticipantControllerUser::class, 'index'])->name('index');
+    });
 
     Route::middleware(['role:participant|instructor'])->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
