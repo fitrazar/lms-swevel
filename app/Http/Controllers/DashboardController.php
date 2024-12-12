1287c6c2ae0165b7d7811de2a1186684b397119d
Zar <fitra6751@gmail.com>
2024-12-12 18:51:17 +0700

    feat: add search & filter course in dashboard participant

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,23 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use App\Models\Enrollment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $search = null;
+        $filter = null;
         if (Auth::user()->roles->pluck('name')[0] == 'participant') {
-            $activeCourses = Enrollment::where('participant_id', Auth::user()->participant->id)
-                ->with('course')
-                ->get()
-                ->pluck('course');
+            $search = $request->input('search');
+            $filter = $request->input('filter');
+            $activeCourses = Course::query()
+                ->when($search, function ($query, $search) {
+                    return $query->where('title', 'like', "%{$search}%");
+                })
+                ->when($filter == 'active', function ($query) {
+                    return $query->whereHas('enrolls', function ($query) {
+                        $query->where('status', 'active');
+                    });
+                })
+                ->when($filter == 'inactive', function ($query) {
+                    return $query->whereHas('enrolls', function ($query) {
+                        $query->where('status', 'inactive');
+                    });
+                })
+                ->whereHas('enrolls', function ($query) {
+                    $query->where('participant_id', Auth::user()->participant->id);
+                })
+                ->latest()
+                ->paginate(9);
+
         } else {
             $activeCourses = collect();
         }
 
-        return view('dashboard', compact('activeCourses'));
+        return view('dashboard', compact('activeCourses', 'search', 'filter'));
     }
 }
--- resources/views/partials/participant.blade.php
+++ resources/views/partials/participant.blade.php
@@ -1,4 +1,28 @@
-<section class="mt-5">
+<section class="py-12">
+    <div class="grid gap-4">
+        <x-form method="GET" action="{{ route('dashboard.index') }}" class="mb-8 relative">
+            <div class="flex items-center space-x-4">
+                <x-input.text-input type="search" name="search" placeholder="Cari Kursus..." :value="$search"
+                    class="w-full" />
+            </div>
+            <div class="flex justify-between items-center mt-3">
+                <div>
+                    <p class="font-bold">Ditampilkan {{ $activeCourses->count() }} - {{ $activeCourses->total() }}</p>
+                </div>
+                <div class="flex items-center">
+                    <span class="mr-2">Filter :</span>
+                    <select class="select select-bordered w-full max-w-xs" name="filter" id="filter">
+                        <option value="" selected disabled>Pilih Status</option>
+                        <option value="active">Aktif</option>
+                        <option value="inactive">Belum Aktif</option>
+                    </select>
+                </div>
+            </div>
+            <x-button.primary-button type="submit">Cari</x-button.primary-button>
+        </x-form>
+
+    </div>
+
     <div class="py-8 mt-1">
         <h2 class="text-center py-4 font-bold md:text-2xl text-lg">Kursus Saya</h2>
         <div class="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-6 p-4">
@@ -66,12 +90,17 @@
             @empty
                 <x-card.card-default class="static md:col-span-2 lg:col-span-3 col-span-1">
                     <div class="flex flex-col w-full border-opacity-50">
-                        <div class="grid h-20 card bg-base-300 rounded-box place-items-center">Anda belum terdaftar di
-                            kursus manapun
+                        <div class="grid h-20 card bg-base-300 rounded-box place-items-center">Kursus Tidak Ditemukan
                         </div>
                     </div>
                 </x-card.card-default>
             @endforelse
+        </div>
+
+        <div class="mt-6 text-center">
+            <div class="join">
+                {{ $activeCourses->appends(['search' => $search, 'filter' => $filter])->links() }}
+            </div>
         </div>
     </div>
 </section>
