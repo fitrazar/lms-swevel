a04b91ce54841d1016445b55c4168f807638cb0e
Zar <fitra6751@gmail.com>
2024-12-19 12:17:57 +0700

    feat: add softDelete to all table & model

--- resources/views/partials/instructor.blade.php
+++ resources/views/partials/instructor.blade.php
@@ -4,26 +4,26 @@
 </x-card.card-custom>
 @if (auth()->user()->instructor?->courses?->count() > 0)
     <div class="grid grid-cols-1 md::grid-cols-2 lg:grid-cols-3 gap-5 mt-6">
-        <x-card.card-custom class="static glass chart-container">
+        <x-card.card-custom class="static glass chart-container col-span-3">
             <h5 class="mb-2 text-lg font-bold tracking-tight">Peserta Aktif & Belum Aktif</h5>
             <hr>
             <canvas id="courseStatus"></canvas>
         </x-card.card-custom>
-        <x-card.card-custom class="static glass chart-container col-span-2">
+        {{-- <x-card.card-custom class="static glass chart-container col-span-2">
             <h5 class="mb-2 text-lg font-bold tracking-tight">Total Peserta</h5>
             <hr>
             <canvas id="courseEnroll"></canvas>
-        </x-card.card-custom>
+        </x-card.card-custom> --}}
         <x-card.card-custom class="static glass chart-container col-span-3">
             <h5 class="mb-2 text-lg font-bold tracking-tight">Total Peserta (Bar)</h5>
             <hr>
             <canvas id="courseEnroll2"></canvas>
         </x-card.card-custom>
-        <x-card.card-custom class="static glass chart-container col-span-3">
+        {{-- <x-card.card-custom class="static glass chart-container col-span-3">
             <h5 class="mb-2 text-lg font-bold tracking-tight">Kursus Selesai</h5>
             <hr>
             <canvas id="courseDone"></canvas>
-        </x-card.card-custom>
+        </x-card.card-custom> --}}
 
     </div>
 @endif
